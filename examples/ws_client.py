485820739fbda59fba2979282cdc8753472d6bee
Yuan Shuai <jcsyuanshuai@qq.com>
2021-11-15 08:38:28 +0800

    added subscribe to ws

--- examples/ws_client.py
+++ examples/ws_client.py
@@ -9,7 +9,7 @@ async def main():
     async with session.ws_connect('http://127.0.0.1:8080/ws') as ws:
         async for msg in ws:
             print(msg.data)
-            await asyncio.sleep(5)
+            # await asyncio.sleep(1)
             if msg.type == aiohttp.WSMsgType.TEXT:
                 if msg.data == 'close cmd':
                     await ws.close()
